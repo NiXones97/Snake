7f66a9d2c1bbf589b7c7e7969b58cb8b4c3ca8e6
Kacper Szyjka <kacper.szyjka97@gmail.com>
2022-11-11 21:31:00 +0100

    Project Loading...3

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -55,14 +55,13 @@ public class GamePanel extends JPanel implements ActionListener {
         g.setColor(Color.RED);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
 
-        for(int i = 0 ; i < bodyParts ; i++){
-            if (i == 0){
+        for (int i = 0; i < bodyParts; i++) {
+            if (i == 0) {
                 g.setColor(Color.GREEN);
-                g.fillRect(x[i],y[i], UNIT_SIZE, UNIT_SIZE);
-            }
-            else {
-                g.setColor(new Color(45,180,0));
-                g.fillRect(x[i] , y[i] , UNIT_SIZE , UNIT_SIZE);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            } else {
+                g.setColor(new Color(45, 180, 0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
             }
         }
 
@@ -93,51 +92,46 @@ public class GamePanel extends JPanel implements ActionListener {
             case 'R':
                 x[0] = x[0] + UNIT_SIZE;
                 break;
-
-
-
         }
-
-
     }
 
     public void checkApple() {
+        if (( x[0] == appleX) && (y[0] == appleY)) {
+            bodyParts++;
+            applesEaten++;
+            newApple();
+        }
 
     }
 
     public void checkCollisions() {
-
         //checks if head collides with body
-        for(int i = bodyParts; i > 0; i--) {
-            if ((x[0] == x[i] && (y[0] == y[i])))
-            {
+        for (int i = bodyParts; i > 0; i--) {
+            if ((x[0] == x[i] && (y[0] == y[i]))) {
                 running = false;
             }
         }
         //check if head touches left boreder
-        if (x[0] < 0 ) {
+        if (x[0] < 0) {
             running = false;
         }
 
         //check if head touches right border
-        if (x[0] > SCREEN_WIDTH){
+        if (x[0] > SCREEN_WIDTH) {
             running = false;
         }
         //check if head touches top border
-        if (y[0] < 0){
+        if (y[0] < 0) {
             running = false;
         }
         //check if head touches bottom border
-        if (y[0] > SCREEN_HEIGT){
+        if (y[0] > SCREEN_HEIGT) {
             running = false;
         }
 
-        if(!running) {
+        if (!running) {
             timer.stop();
         }
-       // 30 min 11 sec
-
-
     }
 
     public void gameOver(Graphics g) {
@@ -146,24 +140,40 @@ public class GamePanel extends JPanel implements ActionListener {
 
     public void actionPerformed(ActionEvent e) {
 
-        if(running) {
+        if (running) {
             move();
             checkApple();
             checkCollisions();
         }
         repaint();
-
-
     }
 
     public class MyKeyAdapter extends KeyAdapter {
         @Override
         public void keyPressed(KeyEvent e) {
+            switch (e.getKeyCode()) {
+                case KeyEvent.VK_LEFT:
+                    if (direction != 'R') {
+                        direction = 'L';
+                    }
+                    break;
+                case KeyEvent.VK_RIGHT:
+                    if (direction != 'L') {
+                        direction = 'R';
+                    }
+                    break;
+                case KeyEvent.VK_UP:
+                    if (direction != 'D') {
+                        direction = 'U';
+                    }
+                    break;
+                case KeyEvent.VK_DOWN:
+                    if (direction != 'U') {
+                        direction = 'D';
+                    }
+                    break;
+            }
 
         }
-
-
     }
-
-
 }
